3e6da11ee22ce10dbdf0a4ea648fcaaf3ebe4f8d
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-26 10:51:04 +0200

    feat: NavBar works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './components/misc/login/login.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
 ];
